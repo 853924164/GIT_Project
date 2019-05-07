d553d9c78077031607b07abca4acfaa82ebe89f3
morendong <45411847+853924164@users.noreply.github.com>
2019-05-07 17:42:51 +0800

    Update StudentModel.java

--- student/src/com/student/model/StudentModel.java
+++ student/src/com/student/model/StudentModel.java
@@ -2,9 +2,9 @@ package com.student.model;
 
 public class StudentModel {
    
-	 private int id;
-	 private String username;
-	 private String password;
+	 private int id;//id
+	 private String username;//用户名
+	 private String password;//密码
 	 private int sex;
 	 private String love;
 	 
